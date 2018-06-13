a7203ea66ed059d98739829a73d9015fcdab917c
lichaoi <lichaoi@yonyou.com>
2018-06-13 21:59:08 +0800

    numpy 的属性

---
+++ com/lc/numpy/NumpyDemo1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+array = np.array([[1,2,3],[4,5,6]])
+
+print(array)
+
+print("\n 维度 ndim：",array.ndim)
+
+print("\n 行数和列数 shape : ",array.shape)
+
+print("\n 元素个数 size : ",array.size)
