d2d7c8e994299180a2ef458dbfa65b1a3c07ebd5
lichaoi <lichaoi@yonyou.com>
2018-06-20 19:51:23 +0800

    定义网络

--- com/lc/pytorch/test/PyTorchDemo1.py
+++ com/lc/pytorch/test/PyTorchDemo1.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 
 class Net(nn.Module):
-    def __int__(self):
+    def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
@@ -15,18 +15,12 @@ class Net(nn.Module):
     def forward(self, x):
         x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
         x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-        x = x.view(-1,self.num_flat_features(x))
+        x = x.view(x.size()[0], -1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
-    def num_flat_features(self, x):
-        size = x.size()[1:]  # all dimensions except the batch dimension
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
 
 net = Net()
 print(net)
