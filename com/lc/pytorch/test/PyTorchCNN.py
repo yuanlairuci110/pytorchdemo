2a22c1397aee6fc28b372327a9bf0fe6c3094d30
lichaoi <lichaoi@yonyou.com>
2018-06-25 22:50:21 +0800

    CNN 卷积神经网络

--- com/lc/pytorch/test/PyTorchCNN.py
+++ com/lc/pytorch/test/PyTorchCNN.py
@@ -3,21 +3,148 @@ import torch.nn as nn
 import torch.utils.data as Data
 import torchvision  # 数据库模块
 import matplotlib.pyplot as plt
+from matplotlib import cm
 
 # 设置随机数种子，保证在不同的计算机上运行时下面的输出一致
 torch.manual_seed(1)
 
 # 超参数
-EPOCH = 1           # 训练整批数据多少次, 为了节约时间, 我们只训练一次
+EPOCH = 1  # 训练整批数据多少次, 为了节约时间, 我们只训练一次
 BATCH_SIZE = 50
-LR = 0.001          # 学习率
+LR = 0.001  # 学习率
 DOWNLOAD_MNIST = False  # 如果你已经下载好了mnist数据就写上 False
 
 train_data = torchvision.datasets.MNIST(
-    root='./mnist/',    # 保存或者提取位置
+    root='./mnist/',  # 保存或者提取位置
     train=True,  # this is training data
-    transform=torchvision.transforms.ToTensor(),    # 转换 PIL.Image or numpy.ndarray 成
-                                                    # torch.FloatTensor (C x H x W), 训练的时候 normalize 成 [0.0, 1.0] 区间
-    download=DOWNLOAD_MNIST          # 没下载就下载, 下载了就不用再下了
+    transform=torchvision.transforms.ToTensor(),  # 转换 PIL.Image or numpy.ndarray 成
+    # torch.FloatTensor (C x H x W), 训练的时候 normalize 成 [0.0, 1.0] 区间
+    download=DOWNLOAD_MNIST  # 没下载就下载, 下载了就不用再下了
 )
 
+# 黑色的地方的值都是0, 白色的地方值大于0
+# 打印一个实例
+print(train_data.train_data.size())  # (60000, 28, 28)
+print(train_data.train_labels.size())  # (60000)
+plt.imshow(train_data.train_data[0].numpy(), cmap='gray')
+plt.title('%i' % train_data.train_labels[0])
+plt.show()
+
+# 加载训练数据
+train_loader = Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
+
+test_data = torchvision.datasets.MNIST(root='./mnist/', train=False)
+# 一个数据
+one_x = torch.unsqueeze(test_data.test_data, dim=1).type(torch.FloatTensor)[
+        :1] / 255.
+one_y = test_data.test_labels[:1]
+
+one_x_numpy = one_x.numpy()
+
+one_y_numpy = one_y.numpy()
+
+print("one_x_numpy : ", one_x_numpy)
+
+print("one_y_numpy : ", one_y_numpy)
+
+# 2000个测试数据
+test_x = torch.unsqueeze(test_data.test_data, dim=1).type(torch.FloatTensor)[
+         :2000] / 255.  # shape from (2000, 28, 28) to (2000, 1, 28, 28), value in range(0,1)
+test_y = test_data.test_labels[:2000]
+
+
+# 定义神经网络
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=16,
+                kernel_size=5,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, 5, 1, 2),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.out = nn.Linear(32 * 7 * 7, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(x.size(0), -1)
+        output = self.out(x)
+        return output, x
+
+
+cnn = CNN()
+
+# CNN 流程
+#  1）. 卷积(Conv2d) -> 激励函数(ReLU) -> 池化, 向下采样 (MaxPooling) ->
+#  2). 再来一遍 ->
+#  3). 展平多维的卷积成的特征图 ->
+#  4). 接入全连接层 (Linear) ->
+#  5). 输出
+print(cnn)
+
+# 优化器
+optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)
+loss_func = nn.CrossEntropyLoss()
+
+try:
+    from sklearn.manifold import TSNE;
+
+    HAS_SK = True
+except:
+    HAS_SK = False;
+    print('Please install sklearn for layer visualization')
+
+
+def plot_with_labels(lowDWeights, labels):
+    plt.cla()
+    X, Y = lowDWeights[:, 0], lowDWeights[:, 1]
+    for x, y, s in zip(X, Y, labels):
+        c = cm.rainbow(int(255 * s / 9));
+        plt.text(x, y, s, backgroundcolor=c, fontsize=9)
+    plt.xlim(X.min(), X.max());
+    plt.ylim(Y.min(), Y.max());
+    plt.title('Visualize last layer');
+    plt.show();
+    plt.pause(0.01)
+
+
+plt.ion()
+
+# 训练和测试
+for epoch in range(EPOCH):
+    for step, (b_x, b_y) in enumerate(train_loader):
+        output = cnn(b_x)[0]
+        loss = loss_func(output, b_y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # 测试
+        if step % 50 == 0:
+            test_output, last_layer = cnn(test_x)
+            pred_y = torch.max(test_output, 1)[1].data.squeeze()
+            accuracy = float(sum(pred_y == test_y)) / float(test_y.size(0))
+            print('Epoch: ', epoch, '| train loss: %.4f' % loss.data.numpy(), '| test accuracy: %.3f' % accuracy)
+            if HAS_SK:
+                tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+                plot_only = 500
+                low_dim_embs = tsne.fit_transform(last_layer.data.numpy()[:plot_only, :])
+                labels = test_y.numpy()[:plot_only]
+                plot_with_labels(low_dim_embs, labels)
+plt.ioff()
+
+test_output, _ = cnn(test_x[:10])
+pred_y = torch.max(test_output, 1)[1].data.numpy().squeeze()
+print(pred_y, 'prediction number')
+print(test_y[:10].numpy(), 'real number')
