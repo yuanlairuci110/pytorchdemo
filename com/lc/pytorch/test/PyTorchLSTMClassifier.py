2de6ce2f411907031eac4a6e1e5c53c2ea518287
lichaoi <lichaoi@yonyou.com>
2018-07-01 14:49:17 +0800

    1

--- com/lc/pytorch/test/PyTorchLSTMClassifier.py
+++ com/lc/pytorch/test/PyTorchLSTMClassifier.py
@@ -13,7 +13,7 @@ INPUT_SIZE = 28
 LR = 0.01
 DOWNLOAD_MNIST = False
 
-# 加载数据
+# 加载训练数据
 train_data = dsets.MNIST(
     root='./mnist/',
     train=True,
@@ -22,4 +22,62 @@ train_data = dsets.MNIST(
 )
 
 # 打印一个实例
-print(train_data.train_data.size())
+print(train_data.train_data.size())  # 测试数据的大小
+print(train_data.train_labels.size())  # 测试数据的类别
+plt.imshow(train_data.train_data[12].numpy(), cmap='gray')
+plt.title('%i' % train_data.train_labels[12])
+plt.show()
+
+# 加载训练数据
+train_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
+
+
+# 加载测试数据
+test_data = dsets.MNIST(root="./mnist", train=False, transform=transforms.ToTensor())
+test_x = test_data.test_data.type(torch.FloatTensor)[:2000] / 255
+test_y = test_data.test_labels.numpy().squeeze()[:2000]
+
+# print(test_x[0])
+# print(test_y)
+
+class RNN(nn.Module):
+    def __init__(self):
+        super(RNN, self).__init__()
+        self.rnn = nn.LSTM(
+            input_size=INPUT_SIZE,
+            hidden_size=64,
+            num_layers=1,
+            batch_first=True
+        )
+        self.out = nn.Linear(64, 10)
+
+    def forward(self, x):
+        r_out, (h_n, h_x) = self.rnn(x, None)
+
+        out = self.out(r_out[:, -1, :])
+        return out
+
+
+rnn = RNN()
+
+print(rnn)
+
+optimizer = torch.optim.Adam(rnn.parameters(), lr=LR)
+loss_func = nn.CrossEntropyLoss()
+
+# 训练、测试和保存模型
+for epoch in range(EPOCH):
+    for step, (b_x, b_y) in enumerate(train_loader):        # gives batch data
+        b_x = b_x.view(-1, 28, 28)              # reshape x to (batch, time_step, input_size)
+
+        output = rnn(b_x)                               # rnn output
+        loss = loss_func(output, b_y)                   # cross entropy loss
+        optimizer.zero_grad()                           # clear gradients for this training step
+        loss.backward()                                 # backpropagation, compute gradients
+        optimizer.step()                                # apply gradients
+
+        if step % 50 == 0:
+            test_output = rnn(test_x)                   # (samples, time_step, input_size)
+            pred_y = torch.max(test_output, 1)[1].data.numpy().squeeze()
+            accuracy = float((pred_y == test_y).astype(int).sum()) / float(test_y.size)
+            print('Epoch: ', epoch, '| train loss: %.4f' % loss.data.numpy(), '| test accuracy: %.2f' % accuracy)
