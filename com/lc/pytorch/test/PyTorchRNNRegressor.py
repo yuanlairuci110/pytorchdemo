a9f0cec9444b54eb800de75f12cdd451a319a131
lichaoi <lichaoi@yonyou.com>
2018-07-01 16:16:21 +0800

    定义模型

--- com/lc/pytorch/test/PyTorchRNNRegressor.py
+++ com/lc/pytorch/test/PyTorchRNNRegressor.py
@@ -23,3 +23,34 @@ plt.plot(steps, y_np, 'r-', label='target(cos)')
 plt.plot(steps, x_np, 'b-', label='input(sin)')
 plt.legend(loc='best')
 plt.show()
+
+
+class RNN(nn.Module):
+    def __init__(self):
+        super(RNN, self).__init__()
+        self.rnn = nn.RNN(
+            input_size=INPUT_SIZE,
+            hidden_size=32,
+            num_layers=1,
+            batch_first=True
+        )
+        self.out = nn.Linear(32, 1)
+
+    def forward(self, x, h_state):
+        # x (batch, time_step, input_size)
+        # h_state (n_layers, batch, hidden_size)
+        # r_out (batch, time_step, hidden_size)
+        # h_state 也要作为RNN的一个输入
+        r_out, h_state = self.rnn(x, h_state)
+        # 保存所有时间点的预测值
+        outs = []
+        # 对每一个时间点计算 output
+        for time_step in range(r_out.size(1)):
+            outs.append(self.out(r_out[:, time_step, :]))
+        # 显示outs
+        # print("---------outs---------------\n",outs)
+        return torch.stack(outs, dim=1), h_state
+
+
+rnn = RNN()
+print(rnn)
