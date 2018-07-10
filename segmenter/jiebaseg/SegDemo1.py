06a986f89bce574b668c0b67da09bc3a8637e6f8
lichaoi <lichaoi@yonyou.com>
2018-07-10 22:48:04 +0800

    去停用词

--- segmenter/jiebaseg/SegDemo1.py
+++ segmenter/jiebaseg/SegDemo1.py
@@ -1,5 +1,6 @@
 # encoding=utf-8
 import jieba
+import jieba.analyse as analyse
 
 # ==========================分词=====================
 
@@ -27,7 +28,22 @@ jieba.load_userdict("userdict.txt")
 seg_list = jieba.cut(str)
 print("加载字典 ："+", ".join(seg_list))
 
-# 程序添加自定义词典词
+# 加载停用词
+#把停用词做成字典
+stopwords = {}
+fstop = open('stop_words.txt', 'r',encoding="utf-8")
+for eachWord in fstop:
+    stopwords[eachWord]=eachWord
+fstop.close()
+wordList = list(jieba.cut(str))
+outStr = ''
+for word in wordList:
+    if word not in stopwords:
+        outStr += word
+        outStr += ' '
+print("去停用词后的分词 ：",outStr)
+
+    # 程序添加自定义词典词
 jieba.add_word(("刘诗诗"))
 jieba.add_word(("演员假"))
 seg_list = jieba.cut(str)
@@ -41,3 +57,14 @@ jieba.suggest_freq(('演', '员'), True)
 seg_list = jieba.cut(str)
 print("加载程序字典 ："+", ".join(seg_list))
 
+#=================TF-IDF关键词提取==============
+text = '新媒体运营如何提升自己的写作能力'
+# 添加新词
+word = '新媒体运营'
+jieba.suggest_freq((word), True)
+jieba.add_word(word, freq=100, tag='get')
+# 利用idf进行关键词提取
+# jieba.analyse.set_idf_path("/idf.txt.big")
+print (' '.join(jieba.analyse.extract_tags(text, topK=100, withWeight=False, allowPOS=('get','n','ns','vn'))))
+
+#==============基于 TextRank 算法的关键词抽取============
